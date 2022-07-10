fb2aedad1c38962265743daa0cd70c5cb33ef9d3
K-MHOON <azmria117@gmail.com>
2022-07-10 09:58:54 +0900

    Initial commit

---
+++ ex01/src/main/java/com/example/ex01/Ex01Application.java
@@ -0,0 +1,15 @@
+package com.example.ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ex01 = JDBC 이해 학습 Application
+ */
+@SpringBootApplication
+public class Ex01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01Application.class, args);
+    }
+}
