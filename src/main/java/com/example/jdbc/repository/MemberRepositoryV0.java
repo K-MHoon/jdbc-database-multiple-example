4cf7b1b432806bb3413a50082bfc28867e3d984d
K-MHOON <azmria117@gmail.com>
2022-07-11 00:51:36 +0900

    feat : JDBC Repository 생성 기능 구현 및 테스트

---
+++ src/main/java/com/example/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,36 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.connection.DBConnectionUtil;
+import com.example.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        try (Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, member.getMemberId());
+            ps.setInt(2, member.getMoney());
+            ps.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
