6e62114daf01776c9cb94c34989bba8d9664077a
K-MHOON <azmria117@gmail.com>
2022-07-10 10:41:13 +0900

    feat : JDBC Connection 구현, 테스트

---
+++ src/main/java/com/example/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,13 @@
+package com.example.jdbc.connection;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+}
