0885a252ca240f1eb6784db522c4747be939f359
K-MHOON <azmria117@gmail.com>
2022-07-16 09:32:08 +0900

    feat : 트랜잭션이 없는 경우 문제를 발생시키는 테스트

---
+++ src/test/java/com/example/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,72 @@
+package com.example.jdbc.service;
+
+import com.example.jdbc.domain.Member;
+import com.example.jdbc.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static com.example.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제 발생
+ */
+class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepositoryV1;
+    private MemberServiceV1 memberServiceV1;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepositoryV1 = new MemberRepositoryV1(dataSource);
+        memberServiceV1 = new MemberServiceV1(memberRepositoryV1);
+    }
+
+    @Test
+    void 정상_이체() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepositoryV1.save(memberA);
+        memberRepositoryV1.save(memberB);
+
+        memberServiceV1.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        Member findMemberA = memberRepositoryV1.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV1.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    void 이체중_예외_발생() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEX = new Member(MEMBER_EX, 10000);
+        memberRepositoryV1.save(memberA);
+        memberRepositoryV1.save(memberEX);
+
+        assertThatThrownBy(() -> memberServiceV1.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        Member findMemberA = memberRepositoryV1.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV1.findById(memberEX.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepositoryV1.delete(MEMBER_A);
+        memberRepositoryV1.delete(MEMBER_B);
+        memberRepositoryV1.delete(MEMBER_EX);
+    }
+}
