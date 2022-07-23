6823a425f26c9a4c20c5f68a5b588a4e6d09fa31
K-MHOON <azmria117@gmail.com>
2022-07-23 11:15:42 +0900

    fix : 예외 처리 누수 문제 해결
    - SQLException(Exception ex) to MyDbException(Runtime ex)

---
+++ src/main/java/com/example/jdbc/repository/MemberRepositoryV4_1.java
@@ -0,0 +1,131 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.domain.Member;
+import com.example.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ * MemberRepository 인터페이스 사용
+ * throws SQLException 제거
+ */
+@Slf4j
+public class MemberRepositoryV4_1 implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV4_1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, member.getMemberId());
+            ps.setInt(2, member.getMoney());
+            ps.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, memberId);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, ps, rs);
+        }
+        throw new NoSuchElementException("Member not found memberId = " + memberId);
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, memberId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, money);
+            ps.setString(2, memberId);
+
+            int resultSize = ps.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    private void close(Connection con, Statement ps, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(ps);
+        DataSourceUtils.releaseConnection(con, dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        // TransactionSynchronizationManager.getResource(dataSource);
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+    }
+}
